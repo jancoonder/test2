103842ab2a4939dd5dc9a90a38c2111a45c18b14
Jantchi <janco.onder@gmail.com>
2018-12-02 10:59:25 +0100

    Add main files

---
+++ src/sk/test2/struktura/Druhy.java
@@ -0,0 +1,8 @@
+package sk.test2.struktura;
+
+public class Druhy {
+	public String toString()
+	{
+		return "som tu";
+	}
+}
---
+++ src/sk/test2/struktura/hlavny.java
@@ -0,0 +1,13 @@
+package sk.test2.struktura;
+
+public class hlavny 
+{
+
+	public static void main(String[] args) 
+	{
+		Druhy druhy = new Druhy();
+		System.out.println("Druhy je "+druhy.toString());
+
+	}
+
+}
